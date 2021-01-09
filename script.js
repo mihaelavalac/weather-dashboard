26ccc6dc65e67e3747b97df33775c7adb4bb640f
Mihaela <mihaelavalac@yahoo.com>
2021-01-08 22:45:17 -0500

    The information from the screen is deleted after the clear search history list is clicked

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-
   var currentDate = moment().format("M/D/YYYY");
   var userInput;
+  localStorage.setItem("city-name", userInput);
   var weatherApiKey = "fa71042f3bd6f2175c35473b9ffdf2f4";
 
- // The function below creates an html block which displays the weather of the picked city for the next 5 days on the screen. 
+  // The function below creates an html block which displays the weather of the picked city for the next 5 days on the screen.
   function getFiveDaysWeather(obj) {
     var sectionContainer = " ";
     for (let i = 4, j = 0; i < 40; i = i + 8, j++) {
@@ -15,9 +15,8 @@ $(document).ready(function () {
         "https://openweathermap.org/img/w/" +
         obj.list[i].weather[0].icon +
         ".png";
-        tempForecast = Math.floor(1.8 * (obj.list[i].main.temp - 273) + 32)
+      tempForecast = Math.floor(1.8 * (obj.list[i].main.temp - 273) + 32);
       let section = `<div  class='cell medium-4 large-2'>
-
     <div id = 'fiveDayContainer'> <h5 id="current-city">  ${date}  </h5>
     <img id = "icon" src ="${iconURL}"> 
     <p id="temperature"> Temp: ${tempForecast} F </p>
@@ -28,10 +27,9 @@ $(document).ready(function () {
   }
   //The function below creates an list element for each city.
   function displaySearchHistory(city) {
-    var listEl = `<li class="list-group-item"> ${city}</li>`  
-    $("#search-list").append(listEl);
-  };
-// The function below represents the weather for the current day in the chosen city. 
+    var listEl = `<li class="list-group-item"> ${city}</li>`
+    };
+  // The function below represents the weather for the current day in the chosen city.
   function getTodayWeatherSection(
     cityName,
     todayDate,
@@ -41,22 +39,22 @@ $(document).ready(function () {
     cityWindSpeed,
     cityUV
   ) {
-
     return `<h1 id="current-city">${cityName} ( ${todayDate} ) <img id = "imgIcon" src ="${imgURL}"> </h1>  
 <p id="temperature">Temperature: ${cityTemp} F </p>
 <p id="humidity">Humidity: ${cityHumid}</p>
 <p id="wind-speed">Wind Speed:${cityWindSpeed} </p>
-<p id="uv-index">UV Index: ${cityUV}</p>`
-    
+<p id="uv-index">UV Index: ${cityUV}</p>
+<h4 id="five-day-weather" col-12 style='display:block'>Five Day Weather</h4><br>`;
   }
 
-// The Clear Search History button react on the click event and deletes all the cities of the list. 
+  // The Clear Search History button react on the click event and deletes all the cities of the list.
   $("#clear-all").on("click", function () {
     $("#search-list").empty();
-  })
-  
- // This function displays all the content of the introduced city on the screen(add the city to the list, diplays the weather for current day and below the weather for the next five days). 
-  function displayAllContent (city) {
+    $("#page-content").empty();
+  });
+
+  // This function displays all the content of the introduced city on the screen(add the city to the list, diplays the weather for current day and below the weather for the next five days).
+  function displayAllContent(city) {
     $("#city-search").empty();
     $("#current-weather").empty();
     $("#five-day-weather").empty();
@@ -68,38 +66,39 @@ $(document).ready(function () {
     $.ajax({
       url: weatherUrlAPI,
       method: "GET",
-    }).then(function (response) {
-    
-
-      let uvAPI =
-        "http://api.openweathermap.org/data/2.5/uvi?lat=" +
-        response.coord.lat +
-        "&lon=" +
-        response.coord.lon +
-        "&appid=" +
-        weatherApiKey;
-
-      return $.ajax({
-        url: uvAPI,
-        method: "GET",
-      }).then(function (res) {
-        displaySearchHistory(city);
-        var todayWeatherSection = getTodayWeatherSection(
-          response.name,
-          currentDate,
-         "https://openweathermap.org/img/w/" +response.weather[0].icon +".png",
-          (temperature = Math.floor(1.8 * (response.main.temp - 273) + 32)),
-          response.main.humidity,
-          response.wind.speed,
-          res.value
-
-        );
-        $("#current-weather").append(todayWeatherSection);
-      });
-    }).catch(function (error){
-      console.log(error)
-      console.log("eroare")
     })
+      .then(function (response) {
+        let uvAPI =
+          "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+          response.coord.lat +
+          "&lon=" +
+          response.coord.lon +
+          "&appid=" +
+          weatherApiKey;
+
+        return $.ajax({
+          url: uvAPI,
+          method: "GET",
+        }).then(function (res) {
+          displaySearchHistory(city);
+          var todayWeatherSection = getTodayWeatherSection(
+            response.name,
+            currentDate,
+            "https://openweathermap.org/img/w/" +
+              response.weather[0].icon +
+              ".png",
+            (temperature = Math.floor(1.8 * (response.main.temp - 273) + 32)),
+            response.main.humidity,
+            response.wind.speed,
+            res.value
+          );
+          $("#current-weather").append(todayWeatherSection);
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log("eroare");
+      });
 
     //The section that display the weather for the next 5 days.
     var fiveDaysForecastAPI =
@@ -110,17 +109,19 @@ $(document).ready(function () {
     $.ajax({
       url: fiveDaysForecastAPI,
       method: "GET",
-    }).then(function (data) {
-      $("#five-day").empty();
-      var displayFiveDayWeather = getFiveDaysWeather(data);
-      //iconURL = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
-      $("#five-day").append(displayFiveDayWeather);
-    }).catch(function (error){
-      console.log(error);
     })
-  };
+      .then(function (data) {
+        $("#five-day").empty();
+        var displayFiveDayWeather = getFiveDaysWeather(data);
+        //iconURL = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
+        $("#five-day").append(displayFiveDayWeather);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   // The function below displays the content on the page of a chosen city from the list ( the current day weather and the next five days wether).
-  function displayListContent (city) {
+  function displayListContent(city) {
     $("#city-search").empty();
     $("#current-weather").empty();
     $("#five-day-weather").empty();
@@ -132,37 +133,38 @@ $(document).ready(function () {
     $.ajax({
       url: weatherUrlAPI,
       method: "GET",
-    }).then(function (response) {
-    
-
-      let uvAPI =
-        "http://api.openweathermap.org/data/2.5/uvi?lat=" +
-        response.coord.lat +
-        "&lon=" +
-        response.coord.lon +
-        "&appid=" +
-        weatherApiKey;
-
-      return $.ajax({
-        url: uvAPI,
-        method: "GET",
-      }).then(function (res) {
-        var todayWeatherSection = getTodayWeatherSection(
-          response.name,
-          currentDate,
-         "https://openweathermap.org/img/w/" +response.weather[0].icon +".png",
-          (temperature = Math.floor(1.8 * (response.main.temp - 273) + 32)),
-          response.main.humidity,
-          response.wind.speed,
-          res.value
-
-        );
-        $("#current-weather").append(todayWeatherSection);
-      });
-    }).catch(function (error){
-      console.log(error)
-      console.log("eroare")
     })
+      .then(function (response) {
+        let uvAPI =
+          "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+          response.coord.lat +
+          "&lon=" +
+          response.coord.lon +
+          "&appid=" +
+          weatherApiKey;
+
+        return $.ajax({
+          url: uvAPI,
+          method: "GET",
+        }).then(function (res) {
+          var todayWeatherSection = getTodayWeatherSection(
+            response.name,
+            currentDate,
+            "https://openweathermap.org/img/w/" +
+              response.weather[0].icon +
+              ".png",
+            (temperature = Math.floor(1.8 * (response.main.temp - 273) + 32)),
+            response.main.humidity,
+            response.wind.speed,
+            res.value
+          );
+          $("#current-weather").append(todayWeatherSection);
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log("eroare");
+      });
 
     //The section that display the weather for the next 5 days.
     var fiveDaysForecastAPI =
@@ -173,21 +175,26 @@ $(document).ready(function () {
     $.ajax({
       url: fiveDaysForecastAPI,
       method: "GET",
-    }).then(function (data) {
-      $("#five-day").empty();
-      var displayFiveDayWeather = getFiveDaysWeather(data);
-      $("#five-day").append(displayFiveDayWeather);
-    }).catch(function (error){
-      console.log(error);
     })
-  };
-//The search icon reacts on the click event and display the content on the screen.
+      .then(function (data) {
+        $("#five-day").empty();
+        var displayFiveDayWeather = getFiveDaysWeather(data);
+        $("#five-day").append(displayFiveDayWeather);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  //The search icon reacts on the click event and display the content on the screen.
   $("#run-search").on("click", function (event) {
     userInput = $("#city-search").val().trim();
-    displayAllContent(userInput)
-  });  
-// The list text content (city name) reacts on the click event and displays the current weather an the next five days weather.
+    displayAllContent(userInput);
+    $("input").val("");
+  });
+
+  // The list text content (city name) reacts on the click event and displays the current weather an the next five days weather.
   $(".list-group").on("click", function (event) {
-    displayListContent (event.originalEvent.srcElement.outerText)
+    displayListContent(event.originalEvent.srcElement.outerText);
   });
 });
